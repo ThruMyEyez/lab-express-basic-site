35a8f4854b4c3979e81585c62b165e6b3f7bb92f
Artur Baumeister <Artur.baumeister@gmail.com>
2022-09-24 16:57:11 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const colors = require("colors");
+const hbs = require("hbs");
+const express = require("express"),
+  app = express(),
+  port = 3030;
+
+const { homeData, workData, galleryData, aboutData } = require("./src/data");
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (request, response, next) => {
+  response.render("home", homeData);
+});
+
+app.get("/about", (request, response) => {
+  response.render("about", aboutData);
+});
+
+app.get("/works", (request, response) => {
+  response.render("works", workData);
+});
+
+//app.get("/gallery", (request, response) => {
+//  response.render("gallery");
+//});
+
+app.listen(port, () => console.log(`${colors.yellow.underline("server listening to port: ")} ${port}`));
+
+/* create = app.post()
+  read = app.get()
+  update = app.put()
+  delete = app.delete()
+ */
